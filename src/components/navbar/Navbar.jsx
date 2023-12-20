80f20d1a405b723ac1b87b45330d41b87b785359
Divyanshu Malik <gamerboydivz@gmail.com>
2023-12-20 15:19:12 -0800

    ResilientDB logo changed

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
 import logo from '../../logo.svg';
 import './navbar.css';
-import Resilientdb from '../../assets/Resilientdb.png'
+import Resilientdb from '../../assets/Resilientdb.svg'
 
 const Navbar = () => {
   const [toggleMenu, setToggleMenu] = useState(false);
